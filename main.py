0ee04c1b8b15b7f0619c7f12b8220c3e740e2762
Matevž Jekovec <matevz@oasisprotocol.org>
2026-01-23 12:23:07 +0100

    Add support for topping up ROFL machines

--- main.py
+++ main.py
@@ -12,8 +12,12 @@ load_dotenv()
 # Read and parse WATCH_ADDRESSES environment variable
 watch_addresses = os.getenv('WATCH_ADDRESSES', '').split(',')
 secret_keys = os.getenv('SECRET_KEYS', '').split(',')
-threshold = float(os.getenv('THRESHOLD', 10))
-amount = float(os.getenv('TOPUP_AMOUNT', 100))
+balance_threshold = float(os.getenv('BALANCE_THRESHOLD', 10))
+balance_amount = float(os.getenv('BALANCE_TOPUP_AMOUNT', 100))
+watch_rofl_machines = os.getenv('WATCH_ROFL_MACHINES', '').split(',')
+rofl_threshold = int(os.getenv('ROFL_THRESHOLD', 1800))
+rofl_topup_term = int(os.getenv('ROFL_TOPUP_TERM', "hour"))
+rofl_topup_count = int(os.getenv('ROFL_TOPUP_COUNT', 1))
 
 def parse_secret_keys(secret_keys: list[str]) -> dict[str, tuple[str, str]]:
     sk_map = {}
@@ -44,9 +48,9 @@ def main():
 
             result = exec_oasis(f"account show {a[1]} --network {network} {"--paratime "+pt if pt!="consensus" else "--no-paratime"} --format json")
             if result.returncode == 0 and result.stdout:
-                acc = {}
+                mOut = {}
                 try:
-                    acc = json.loads(result.stdout)
+                    mOut = json.loads(result.stdout)
                 except json.JSONDecodeError as e:
                     print(f"Error parsing JSON for {wa}: {e}")
                     continue
@@ -54,13 +58,13 @@ def main():
                 print(f"Error retrieving account data for {wa}: {result.stderr}")
                 continue
 
-            balance = int(acc["general"]["balance"]) * 10**(-9)
+            balance = int(mOut["general"]["balance"]) * 10**(-9)
             if pt!="consensus":
-                balance = int(acc["paratime_balances"][""]) * 10**(-18)
+                balance = int(mOut["paratime_balances"][""]) * 10**(-18)
 
             print(f"Fetched account {wa}: balance {balance}")
-            if balance < int(os.getenv('THRESHOLD', 10)):
-                print(f" Transferring {amount} to {wa}")
+            if balance < int(balance_threshold):
+                print(f" Transferring {balance_amount} to {wa}")
 
                 # Remove preexisting wallet.
                 exec_oasis(f"wallet remove {TMP_WALLET_NAME} -y")
@@ -80,19 +84,74 @@ def main():
                     print(f" error: Secret key import failed: {wallet_import_result.stderr}")
                     continue
 
-                transfer_result = exec_oasis(f"account transfer {amount} {a[1]} --network {network} {"--paratime "+pt if pt!="consensus" else "--no-paratime"} --account {TMP_WALLET_NAME} -y")
-                if transfer_result.returncode == 0:
-                    print(f" Transfer of {amount} successful to {wa}")
+                topup_result = exec_oasis(f"account transfer {balance_amount} {a[1]} --network {network} {"--paratime " + pt if pt != "consensus" else "--no-paratime"} --account {TMP_WALLET_NAME} -y")
+                if topup_result.returncode == 0:
+                    print(f" Transfer of {balance_amount} successful to {wa}")
                 else:
-                    print(f" Transfer failed to {wa}: {transfer_result.stderr}")
+                    print(f" Transfer failed to {wa}: {topup_result.stderr}")
 
                 # Remove tmp wallet for security.
                 exec_oasis(f"wallet remove {TMP_WALLET_NAME} -y")
             else:
-                print(f" Threshold {threshold} not reached")
+                print(f" Threshold {balance_threshold} not reached")
 
             # Avoid quota limits.
             time.sleep(10)
+
+        for wrm in watch_rofl_machines:
+            m = wrm.split(':', 1)
+            pt, network = (m[0] if '-' in m[0] else m[0]+"-mainnet").split('-')
+
+            result = exec_oasis(f"rofl machine show {m[1]} --network {network} --paratime {pt} --format json")
+            if result.returncode == 0 and result.stdout:
+                mOut = {}
+                try:
+                    mOut = json.loads(result.stdout)
+                except json.JSONDecodeError as e:
+                    print(f"Error parsing JSON for {wa}: {e}")
+                    continue
+            else:
+                print(f"Error retrieving account data for {wa}: {result.stderr}")
+                continue
+
+            paid_until = int(mOut["machine"]["paid_until"])
+
+            print(f"Fetched rofl machine {wrm}: paid_until {paid_until}")
+            if paid_until - rofl_threshold < int(time.time()):
+                print(f" Topping up {wrm} for {rofl_topup_count}x {rofl_topup_term}")
+
+                # Remove preexisting wallet.
+                exec_oasis(f"wallet remove {TMP_WALLET_NAME} -y")
+
+                # Pick the corresponding account.
+                if f"{pt}-{network}" not in sk_map:
+                    print(f" error: Secret key to fund account {wa} not found. Ignoring.")
+                    print(sk_map)
+                    print(network)
+                    continue
+                algorithm, secret = sk_map[f"{pt}-{network}"]
+
+                # Import funding account.
+                wallet_import_result = exec_oasis(f"wallet import {TMP_WALLET_NAME} --algorithm {algorithm} --secret {secret} -y")
+
+                if wallet_import_result.returncode != 0:
+                    print(f" error: Secret key import failed: {wallet_import_result.stderr}")
+                    continue
+
+                topup_result = exec_oasis(f"rofl machine top-up {m[1]} --term {rofl_topup_term} --term-count {rofl_topup_count} --network {network} --paratime {pt} --account {TMP_WALLET_NAME} -y")
+                if topup_result.returncode == 0:
+                    print(f" Top up {wrm} for {rofl_topup_count}x {rofl_topup_term} successful")
+                else:
+                    print(f" Top up failed for {wrm}: {topup_result.stderr}")
+
+                # Remove tmp wallet for security.
+                exec_oasis(f"wallet remove {TMP_WALLET_NAME} -y")
+            else:
+                print(f" Threshold {rofl_threshold}s not reached")
+
+            # Avoid quota limits.
+            time.sleep(10)
+
 
 if __name__ == "__main__":
     main()
