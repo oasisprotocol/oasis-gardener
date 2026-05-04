ca860a291324eb6bedbc596ea828cc5e8cbdc1bd
Matevž Jekovec <matevz@oasisprotocol.org>
2026-05-04 14:08:50 +0200

    main.py: Print addresses of the funding accounts

--- main.py
+++ main.py
@@ -23,14 +23,25 @@ rofl_topup_count = int(os.getenv('ROFL_TOPUP_COUNT', 1))
 def parse_secret_keys(secret_keys: list[str]) -> dict[str, tuple[str, str]]:
     sk_map = {}
     for sk in secret_keys:
-        network, algorithm, secret = sk.split(':')
-        if '-' not in network:
-            network += '-mainnet'
-        sk_map[network] = (algorithm, secret)
+        pt_network, algorithm, secret = sk.split(':', 2)
+        if '-' not in pt_network:
+            pt_network += '-mainnet'
+        pt, network = pt_network.split('-')
+        if not algorithm.endswith('-raw'):
+            print(f"error: {pt}-{network}:{algorithm}: only raw secret keys are supported")
+            exit(-1)
+        sk_map[pt_network] = (algorithm, secret, "")
+
+        # Try importing the secret key and derive the address.
+        if not import_wallet(pt, network, sk_map):
+            print(f"error: {pt}-{network}:{algorithm}: failed to import secret key")
+            exit(-1)
+        account_info = json.loads(exec_oasis(f"account show {TMP_WALLET_NAME} --format json").stdout)
+        sk_map[pt_network] = (algorithm, secret, account_info["ethereum_address"] if "ethereum_address" in account_info else account_info["native_address"])
 
     return sk_map
 
-def import_wallet(pt: str, network: str, sk_map: dict[str, tuple[str, str]]) -> bool:
+def import_wallet(pt: str, network: str, sk_map: dict[str, tuple[str, str, str]]) -> bool:
     # Remove preexisting wallet.
     exec_oasis(f"wallet remove {TMP_WALLET_NAME} -y")
 
@@ -38,7 +49,7 @@ def import_wallet(pt: str, network: str, sk_map: dict[str, tuple[str, str]]) -> 
     if f"{pt}-{network}" not in sk_map:
         print(f" error: Secret key for {pt}-{network} not found.")
         return False
-    algorithm, secret = sk_map[f"{pt}-{network}"]
+    algorithm, secret, _ = sk_map[f"{pt}-{network}"]
 
     # Import funding account.
     wallet_import_result = exec_oasis(f"wallet import {TMP_WALLET_NAME} --algorithm {algorithm} --secret {secret} -y")
@@ -59,7 +70,10 @@ def exec_oasis(params: str):
 
 def main():
     sk_map = parse_secret_keys(secret_keys)
-    print(f"Imported {len(sk_map)} secret keys")
+
+    print(f"Imported {len(sk_map)} secret keys. Funding accounts:")
+    for pt_network in sk_map:
+        print(f" {pt_network}: {sk_map[pt_network][2]}")
 
     while True:
         for wa in watch_addresses:
@@ -134,7 +148,7 @@ def main():
                 print(f" Topping up {wrm} for {rofl_topup_count}x {rofl_topup_term}")
 
                 if not import_wallet(pt, network, sk_map):
-                    print(f" error: Secret key to topup ROFL machine {wrm} not found. Ignoring.")
+                    print(f" error: No valid secret key to topup ROFL machine {wrm} found. Ignoring.")
                     continue
 
                 topup_result = exec_oasis(f"rofl machine top-up {m[1]} --term {rofl_topup_term} --term-count {rofl_topup_count} --network {network} --paratime {pt} --account {TMP_WALLET_NAME} -y")
